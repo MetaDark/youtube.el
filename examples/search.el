88464af3813ffb82fe47d399770466f7b5f5cb0d
MetaDark <kira.bruneau@gmail.com>
2018-07-26 00:49:31 -0400

    Use deferred instead of callbacks

--- examples/search.el
+++ examples/search.el
@@ -8,70 +8,68 @@
 (eval-when-compile
   (require 'dash))
 
-(defmacro lazy-insert-image (image &optional string area slice)
-  `(let ((string ,(or string " "))
-         (area ,area)
-         (slice ,slice)
-         (image-buffer (current-buffer))
-         (image-point (point)))
-     (insert string)
-     ,(append
-       image
-       `((lambda (image)
-           (with-current-buffer image-buffer
-             (save-excursion
-               (let ((inhibit-read-only t))
-                 ;; NOTE: This will get messed up if anything is
-                 ;; inserted before image-point during the load
-                 (goto-char image-point)
-                 (delete-char (length string))
-                 (insert-image image string area slice)))))))))
+(defun deferred:excursion (d callback)
+  (let ((saved-buffer (current-buffer))
+        (saved-marker (point-marker))
+        (saved-inhibit-read-only inhibit-read-only))
+    (deferred:$ d
+      (deferred:nextc it
+        (lambda (result)
+          (with-current-buffer saved-buffer
+            (save-mark-and-excursion
+              (let ((inhibit-read-only saved-inhibit-read-only))
+                (goto-char saved-marker)
+                (funcall callback result)))))))))
 
 (defun youtube-search-interactive (query)
   (interactive "MSearch Query: ")
-  (youtube-search
-   query
-   (lambda (videos)
-     (let ((search-results (get-buffer-create "youtube search results")))
-       (with-current-buffer search-results
-         (let ((inhibit-read-only t))
-           (erase-buffer)
-           (seq-doseq (video videos)
+  (deferred:$
+    (youtube-search query)
+    (deferred:nextc it
+      (lambda (videos)
+        (let ((search-results (get-buffer-create "youtube search results")))
+          (with-current-buffer search-results
+            (let ((inhibit-read-only t))
+              (erase-buffer)
+              (seq-doseq (video videos)
+                (deferred:excursion (youtube-video-thumbnail-image video)
+                  (lambda (image)
+                    (insert-image image)
+                    (newline)))
 
-             (lazy-insert-image (youtube-video-thumbnail-image video))
-             (newline)
+                (let ((video (youtube-video-title video)))
+                  (insert video)
+                  (newline))
 
-             (let ((video (youtube-video-title video)))
-               (insert video)
-               (newline))
+                (let* ((author (youtube-video-author video))
+                       (name (youtube-author-name author))
+                       (url (youtube-author-url author))
+                       (verified (youtube-author-verified author)))
+                  (insert-text-button name 'action (lambda (x) (browse-url url)))
+                  (insert (if verified " ✓" ""))
+                  (newline))
 
-             (let* ((author (youtube-video-author video))
-                    (name (youtube-author-name author))
-                    (url (youtube-author-url author))
-                    (verified (youtube-author-verified author)))
-               (insert-text-button name 'action (lambda (x) (browse-url url)))
-               (insert (if verified " ✓" ""))
-               (newline))
+                (when-let ((meta (list
+                                  (-some-> (youtube-video-views-string video) (concat " views"))
+                                  (youtube-video-date-string video))))
+                  (insert (string-join (seq-filter #'identity meta) " • "))
+                  (newline))
 
-             (when-let ((meta (list
-                               (-some-> (youtube-video-views-string video) (concat " views"))
-                               (youtube-video-date-string video))))
-               (insert (string-join (seq-filter #'identity meta) " • "))
-               (newline))
+                (when-let (description (-some-> (youtube-video-description video)))
+                  (insert description)
+                  (newline))
 
-             (when-let (description (-some-> (youtube-video-description video)))
-               (insert description)
-               (newline))
+                (when-let ((badges (youtube-video-badges video)))
+                  (insert (string-join badges " • "))
+                  (newline))
 
-             (when-let ((badges (youtube-video-badges video)))
-               (insert (string-join badges " • "))
-               (newline))
+                (let ((url (youtube-video-url video)))
+                  (insert-text-button url 'action (lambda (x) (start-process (concat "mpv " url) nil "mpv" url)))
+                  (newline))
 
-             (let ((url (youtube-video-url video)))
-               (insert-text-button url 'action (lambda (x) (start-process (concat "mpv " url) nil "mpv" url)))
-               (newline))
+                (newline)))
+            (goto-char (point-min))
+            (read-only-mode))
+          (switch-to-buffer search-results))))))
 
-             (newline)))
-         (goto-char (point-min))
-         (read-only-mode))
-       (switch-to-buffer search-results)))))
+(youtube-search-interactive "rick astley")
--- youtube.el
+++ youtube.el
@@ -3,13 +3,15 @@
 ;; NOTE: elquery returns results in reverse order, so it is necessary
 ;; to reverse the results to get them in order.
 
+(require 'deferred)
 (require 'elquery)
 (require 'json)
 (require 'map)
 (require 'seq)
 (require 'subr-x)
 (require 'time-date)
-(require 'url-http)
+(require 'url-expand)
+(require 'url-util)
 
 (eval-when-compile
   (require 'cl-macs)
@@ -27,7 +29,9 @@
   (badges nil :read-only t))
 
 (defun youtube-video-url (video)
-  (format "https://www.youtube.com/watch?v=%s" (youtube-video-id video)))
+  (-> "https://www.youtube.com/watch"
+    (youtube--url-with-query
+     `((v ,(youtube-video-id video))))))
 
 (defun youtube-video-date-string (video)
   (youtube-video-date video))
@@ -41,10 +45,9 @@
     (nreverse it)
     (string-join it ",")))
 
-(defun youtube-video-thumbnail-image (video callback)
-  (if-let ((url (youtube-video-thumbnail-url video)))
-      (youtube--download-image url callback)
-    (funcall callback nil)))
+(defun youtube-video-thumbnail-image (video)
+  (-some-> (youtube-video-thumbnail-url video)
+    (youtube--http-get-image)))
 
 (defun youtube-video-duration-string (video)
   (-some--> (youtube-video-duration-seconds video)
@@ -64,27 +67,30 @@
   (url nil :read-only t)
   (mimetype nil :read-only t))
 
-(defun youtube-search (query callback)
-  (url-retrieve
-   ;; TODO: Escape query
-   ;; TODO: Use pbj=1 instead of spf=navigate, it used by the polymer (material design) interface
-   (format "https://www.youtube.com/results?search_query=%s&spf=navigate" query)
-   (lambda (status callback)
-     (goto-char url-http-end-of-headers)
-     (if-let ((results (-some-> (json-read) (youtube--scrape-search-body))))
-         (funcall callback results)
-       (switch-to-buffer-other-window (current-buffer))
-       (error "failed to scrape youtube search results")))
-   (list callback))
-  nil)
+(defun youtube-search (query)
+  ;; TODO: Use pbj=1 instead of spf=navigate, it used by the
+  ;; polymer (material design) interface
+  (let ((url (-> "https://www.youtube.com/results"
+               (youtube--url-with-query
+                `((search_query ,query)
+                  (spf "navigate"))))))
+    (deferred:$
+      (deferred:url-retrieve url)
+      (deferred:nextc it
+        (lambda (buf)
+          (with-current-buffer buf
+            (if-let ((videos (-> (json-read)
+                               (youtube--scrape-search-body url))))
+                videos
+              (error "failed to scrape youtube search results"))))))))
 
-(defun youtube--scrape-search-body (body)
+(defun youtube--scrape-search-body (body url)
   (-some-> body
     (seq-elt 1) (map-elt 'body) (map-elt 'content)
     (elquery-read-string)
-    (youtube--scrape-search-content)))
+    (youtube--scrape-search-content url)))
 
-(defun youtube--scrape-search-content (content)
+(defun youtube--scrape-search-content (content url)
   ;; Total results:
   ;; NOTE: could overflow on 32-bit machines
   ;; (-some->>
@@ -95,9 +101,9 @@
   (->> content
     (elquery-$ ".yt-lockup-video") (nreverse)
     (seq-remove (lambda (video) (elquery-$ ".yt-badge-ad" video)))
-    (seq-map #'youtube--scrape-search-video)))
+    (seq-map (lambda (video) (youtube--scrape-search-video video url)))))
 
-(defun youtube--scrape-search-video (video)
+(defun youtube--scrape-search-video (video url)
   (let* ((thumbnail (->> video (elquery-$ ".yt-lockup-thumbnail") (car)))
          (thumbnail-img (->> thumbnail (elquery-$ "img") (car)))
          (duration (->> thumbnail (elquery-$ ".video-time") (car)))
@@ -111,7 +117,7 @@
     (youtube--video-create
      :id (-> video (elquery-data "context-item-id"))
      :title (-some-> title (elquery-text))
-     :author (-some-> author youtube--scrape-search-author)
+     :author (-some-> author (youtube--scrape-search-author url))
      :date (-some-> date (elquery-text) (youtube--scrape-relative-date))
      ;; NOTE: views could overflow on 32-bit machines (eg. Gangnam Style)
      :views (-some-> views (elquery-text) (youtube--scrape-integer))
@@ -120,18 +126,18 @@
      (-some->
          (or (-some-> thumbnail-img (elquery-data "thumb"))
              (-some-> thumbnail-img (elquery-prop "src")))
-       (url-expand-file-name))
+       (url-expand-file-name url))
      :duration-seconds (-some-> duration (elquery-text) (youtube--scrape-duration-seconds))
      :badges (->> badges (seq-map #'elquery-text)))))
 
-(defun youtube--scrape-search-author (author)
+(defun youtube--scrape-search-author (author url)
   (let ((verified (->> author (elquery-$ ".yt-channel-title-icon-verified") (car))))
     (youtube--author-create
-     :url (-some-> author (elquery-prop "href") (url-expand-file-name))
+     :url (-some-> author (elquery-prop "href") (url-expand-file-name url))
      :name (-> author (elquery-text))
      :verified (if verified t))))
 
-(defun youtube-streams (id callback)
+(defun youtube-streams (id)
   (error "unimplemented"))
 
 (defun youtube--scrape-integer (integer)
@@ -155,17 +161,20 @@
   ;; TODO: Parse into a date object
   date)
 
-(defun youtube--download-image (image callback)
-  (url-retrieve
-   image
-   (lambda (status callback)
-     (--> (buffer-substring-no-properties
-           (+ 1 url-http-end-of-headers) (point-max))
-       ;; TODO: Parse type from HTTP response
-       ;; (or (content-type)
-       ;;     (image-type-from-file-name))
-       (create-image it nil t)
-       (funcall callback it)))
-   (list callback)))
+(defun youtube--url-with-query (url query)
+  (let ((query-string (url-build-query-string query)))
+    (if (string-empty-p query-string) url
+      (concat url "?" query-string))))
+
+(defun youtube--http-get-image (image)
+  (deferred:$
+    (deferred:url-retrieve image)
+    (deferred:nextc it
+      (lambda (buf)
+        (with-current-buffer buf
+          ;; TODO: Parse content type / extension from HTTP response
+          (--> (buffer-substring-no-properties
+                (search-forward "\n\n") (point-max))
+            (create-image it 'jpeg t)))))))
 
 (provide 'youtube)
